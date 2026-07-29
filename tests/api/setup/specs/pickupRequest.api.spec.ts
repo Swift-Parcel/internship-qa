8177dfc93e048a7e892a0fa726cc1fd267a1f3d7
Ronald <ronald.t@tcs.com>
2026-07-29 15:58:14 +0200

    prepare test cases for pickup request

---
+++ tests/api/setup/specs/pickupRequest.api.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from "../setup";
+
+test.describe("test request pickup suite", () => {
+    test("test Pickup Creation, Full Payload & Quote Conversion", async ({
+        api,
+        apiBaseUrl,
+    }) => {});
+
+    test("test negative submissions (past dates, invalid types & partial data)", async ({
+        api,
+        apiBaseUrl,
+    }) => {});
+
+    test("test unconfirmed pickup request limit", async ({
+        api,
+        apiBaseUrl,
+    }) => {});
+
+    test("test same-day service restrictions cutoff & cross-Country", async ({
+        api,
+        apiBaseUrl,
+    }) => {});
+});
