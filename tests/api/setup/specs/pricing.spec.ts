beb882e6c37adce38ffb70599be67b4ad7bdb103
Mencser David Oliver <mencseroliver@gmail.com>
2026-08-07 18:00:29 +0200

    Added automatic finding of customerID (up to 20 users)

--- tests/api/setup/specs/pricing.spec.ts
+++ tests/api/setup/specs/pricing.spec.ts
@@ -1,5 +1,5 @@
-import { send } from "node:process";
 import { expect, test } from "../setup";
+import { APIRequest, APIRequestContext, APIResponse } from "@playwright/test";
 
 type PickupRequestDetails = {
     preferredPickupDate: string;
@@ -9,8 +9,20 @@ type PickupRequestDetails = {
     parcelHeight: number;
     parcelWeight: number;
     preferredTimeSlot: 0 | 1 | 2;
-    senderAddress: number;
-    recipientAddress: number;
+    senderAddress: {
+        city: string;
+        street: string;
+        postal_code: string;
+        country_code: string;
+        street_number: string;
+    };
+    recipientAddress: {
+        city: string;
+        street: string;
+        postal_code: string;
+        country_code: string;
+        street_number: string;
+    };
     recipientName: string;
     serviceType: 0 | 1 | 2;
 };
@@ -19,6 +31,78 @@ function statusCodeCheck(actualStatusCode: number, expectedStatusCode: number) {
     expect
         .soft(actualStatusCode, `Status Code ${expectedStatusCode}`)
         .toBe(expectedStatusCode);
+}
+
+let accessTokenOne: string;
+let accessTokenTwo: string;
+
+test.beforeAll(async ({ request }) => {
+    await request.post(`${process.env.API_BASE_URL}customer`, {
+        data: {
+            email: `pricing${process.env.API_USERNAME}`,
+            fullName: "test trial",
+            phoneNumber: "323232232",
+            password: process.env.API_PASSWORD,
+        },
+    });
+
+    const loginResponse = await request.post(
+        `${process.env.API_BASE_URL}auth/login`,
+        {
+            data: {
+                email: `pricing${process.env.API_USERNAME}`,
+                password: process.env.API_PASSWORD,
+            },
+        },
+    );
+    const loginResponseBody = await loginResponse.json();
+    accessTokenOne = `Bearer ${loginResponseBody.access_token}`;
+
+    await request.post(`${process.env.API_BASE_URL}customer`, {
+        data: {
+            email: `pricing2${process.env.API_USERNAME}`,
+            fullName: "test trial",
+            phoneNumber: "323232232",
+            password: process.env.API_PASSWORD,
+        },
+    });
+
+    const login2Response = await request.post(
+        `${process.env.API_BASE_URL}auth/login`,
+        {
+            data: {
+                email: `pricing2${process.env.API_USERNAME}`,
+                password: process.env.API_PASSWORD,
+            },
+        },
+    );
+
+    const login2ResponseBody = await login2Response.json();
+    accessTokenTwo = login2ResponseBody.access_token;
+});
+
+async function returnCustomerId(
+    email: string,
+    request: APIRequestContext,
+    token: string,
+): Promise<number> {
+    let id = 0;
+    for (let i = 4; i < 20; i++) {
+        const profileResponse = await request.get(
+            `${process.env.API_BASE_URL}customer/${i}`,
+            {
+                headers: { Authorization: token },
+            },
+        );
+        const profileResponseBody = await profileResponse.json();
+        if (profileResponse.status() === 200) {
+            if (profileResponseBody.email === email) {
+                id = profileResponseBody.id;
+                return id;
+            }
+        }
+    }
+    return id;
 }
 
 let pickupRequestDetails: PickupRequestDetails;
@@ -32,8 +116,20 @@ test.beforeEach(async () => {
         parcelHeight: 1,
         parcelWeight: 10.01,
         preferredTimeSlot: 0,
-        senderAddress: 16,
-        recipientAddress: 16,
+        senderAddress: {
+            city: "Budapest",
+            street: "A utca",
+            postal_code: "1111",
+            country_code: "HU",
+            street_number: "3",
+        },
+        recipientAddress: {
+            city: "Budapest",
+            street: "C utca",
+            postal_code: "1111",
+            country_code: "HU",
+            street_number: "5",
+        },
         recipientName: "Test Recipient",
         serviceType: 0,
     };
@@ -45,14 +141,39 @@ test.describe("Pricing tests - positive", {}, () => {
     test(
         "Price calculation with standard same country service and surcharge",
         { tag: "@smoke" },
-        async ({ api, apiBaseUrl }) => {
-            pickupRequestDetails.senderAddress = 18;
-            pickupRequestDetails.recipientAddress = 17;
-            await api.post(`${apiBaseUrl}customer/4/pickup-requests`, {
-                data: pickupRequestDetails,
-            });
+        async ({ request }) => {
+            const customerId = await returnCustomerId(
+                `pricing${process.env.API_USERNAME}`,
+                request,
+                accessTokenOne,
+            );
 
-            const response = await api.post(`${apiBaseUrl}pricing/quotes/1`);
+            ((pickupRequestDetails.senderAddress = {
+                city: "Budapest",
+                street: "A utca",
+                postal_code: "1111",
+                country_code: "HU",
+                street_number: "3",
+            }),
+                (pickupRequestDetails.recipientAddress = {
+                    city: "Szeged",
+                    street: "B utca",
+                    postal_code: "6700",
+                    country_code: "HU",
+                    street_number: "34",
+                }),
+                await request.post(
+                    `${process.env.API_BASE_URL}customer/${customerId}/pickup-requests`,
+                    {
+                        headers: { Authorization: accessTokenOne },
+                        data: pickupRequestDetails,
+                    },
+                ));
+
+            const response = await request.post(
+                `${process.env.API_BASE_URL}pricing/quotes/4`,
+                { headers: { Authorization: accessTokenOne } },
+            );
             const responseBody = await response.json();
             statusCodeCheck(response.status(), 201);
 
@@ -78,20 +199,44 @@ test.describe("Pricing tests - positive", {}, () => {
     test(
         "Price calculation with express cross-country service and surcharge",
         { tag: "@smoke" },
-        async ({ api, apiBaseUrl }) => {
+        async ({ request }) => {
+            const customerId = await returnCustomerId(
+                `pricing${process.env.API_USERNAME}`,
+                request,
+                accessTokenOne,
+            );
+
             pickupRequestDetails.parcelWeight = 30;
             pickupRequestDetails.parcelLength = 120;
             pickupRequestDetails.parcelWidth = 120;
             pickupRequestDetails.parcelHeight = 120;
             pickupRequestDetails.serviceType = 1;
-            pickupRequestDetails.senderAddress = 16;
-            pickupRequestDetails.recipientAddress = 18;
+            ((pickupRequestDetails.senderAddress = {
+                city: "Bratislava",
+                street: "D ulica",
+                postal_code: "82100",
+                country_code: "SK",
+                street_number: "1",
+            }),
+                (pickupRequestDetails.recipientAddress = {
+                    city: "Szeged",
+                    street: "B utca",
+                    postal_code: "6700",
+                    country_code: "HU",
+                    street_number: "34",
+                }),
+                await request.post(
+                    `${process.env.API_BASE_URL}customer/${customerId}/pickup-requests`,
+                    {
+                        headers: { Authorization: accessTokenOne },
+                        data: pickupRequestDetails,
+                    },
+                ));
 
-            await api.post(`${apiBaseUrl}customer/4/pickup-requests`, {
-                data: pickupRequestDetails,
-            });
-
-            const response = await api.post(`${apiBaseUrl}pricing/quotes/2`);
+            const response = await request.post(
+                `${process.env.API_BASE_URL}pricing/quotes/5`,
+                { headers: { Authorization: accessTokenOne } },
+            );
             const responseBody = await response.json();
             statusCodeCheck(response.status(), 201);
 
@@ -117,15 +262,28 @@ test.describe("Pricing tests - positive", {}, () => {
     test(
         "Price calculation with same-day same city service and surcharge",
         { tag: "@smoke" },
-        async ({ api, apiBaseUrl }) => {
+        async ({ request }) => {
+            const customerId = await returnCustomerId(
+                `pricing${process.env.API_USERNAME}`,
+                request,
+                accessTokenOne,
+            );
+
             pickupRequestDetails.serviceType = 2;
             pickupRequestDetails.parcelWeight = 5.01;
 
-            await api.post(`${apiBaseUrl}customer/4/pickup-requests`, {
-                data: pickupRequestDetails,
-            });
+            await request.post(
+                `${process.env.API_BASE_URL}customer/${customerId}/pickup-requests`,
+                {
+                    headers: { Authorization: accessTokenOne },
+                    data: pickupRequestDetails,
+                },
+            );
 
-            const response = await api.post(`${apiBaseUrl}pricing/quotes/3`);
+            const response = await request.post(
+                `${process.env.API_BASE_URL}pricing/quotes/6`,
+                { headers: { Authorization: accessTokenOne } },
+            );
             const responseBody = await response.json();
             statusCodeCheck(response.status(), 201);
 
@@ -153,17 +311,41 @@ test.describe("Pricing tests - positive", {}, () => {
     test(
         "Price calculation with same-day same country service and no surcharge",
         { tag: "@smoke" },
-        async ({ api, apiBaseUrl }) => {
+        async ({ request }) => {
+            const customerId = await returnCustomerId(
+                `pricing${process.env.API_USERNAME}`,
+                request,
+                accessTokenOne,
+            );
+
             pickupRequestDetails.serviceType = 2;
             pickupRequestDetails.parcelWeight = 5;
-            pickupRequestDetails.senderAddress = 17;
-            pickupRequestDetails.recipientAddress = 18;
+            ((pickupRequestDetails.senderAddress = {
+                city: "Szeged",
+                street: "B utca",
+                postal_code: "6700",
+                country_code: "HU",
+                street_number: "34",
+            }),
+                (pickupRequestDetails.recipientAddress = {
+                    city: "Budapest",
+                    street: "A utca",
+                    postal_code: "1111",
+                    country_code: "HU",
+                    street_number: "3",
+                }),
+                await request.post(
+                    `${process.env.API_BASE_URL}customer/${customerId}/pickup-requests`,
+                    {
+                        headers: { Authorization: accessTokenOne },
+                        data: pickupRequestDetails,
+                    },
+                ));
 
-            await api.post(`${apiBaseUrl}customer/4/pickup-requests`, {
-                data: pickupRequestDetails,
-            });
-
-            const response = await api.post(`${apiBaseUrl}pricing/quotes/4`);
+            const response = await request.post(
+                `${process.env.API_BASE_URL}pricing/quotes/7`,
+                { headers: { Authorization: accessTokenOne } },
+            );
             const responseBody = await response.json();
             statusCodeCheck(response.status(), 201);
 
@@ -187,18 +369,41 @@ test.describe("Pricing tests - positive", {}, () => {
     );
 
     test("Price calculation with standard cross-country service and no surcharge", async ({
-        api,
-        apiBaseUrl,
+        request,
     }) => {
+        const customerId = await returnCustomerId(
+            `pricing${process.env.API_USERNAME}`,
+            request,
+            accessTokenOne,
+        );
+
         pickupRequestDetails.parcelWeight = 0.01;
-        pickupRequestDetails.senderAddress = 16;
-        pickupRequestDetails.recipientAddress = 17;
+        ((pickupRequestDetails.senderAddress = {
+            city: "Szeged",
+            street: "B utca",
+            postal_code: "6700",
+            country_code: "HU",
+            street_number: "34",
+        }),
+            (pickupRequestDetails.recipientAddress = {
+                city: "Bratislava",
+                street: "D ulica",
+                postal_code: "82100",
+                country_code: "SK",
+                street_number: "1",
+            }),
+            await request.post(
+                `${process.env.API_BASE_URL}customer/${customerId}/pickup-requests`,
+                {
+                    headers: { Authorization: accessTokenOne },
+                    data: pickupRequestDetails,
+                },
+            ));
 
-        await api.post(`${apiBaseUrl}customer/4/pickup-requests`, {
-            data: pickupRequestDetails,
-        });
-
-        const response = await api.post(`${apiBaseUrl}pricing/quotes/5`);
+        const response = await request.post(
+            `${process.env.API_BASE_URL}pricing/quotes/8`,
+            { headers: { Authorization: accessTokenOne } },
+        );
         const responseBody = await response.json();
         statusCodeCheck(response.status(), 201);
 
@@ -221,45 +426,29 @@ test.describe("Pricing tests - positive", {}, () => {
     test("Price calculation with express same city service and no surcharge", async ({
         request,
     }) => {
-        await request.post(`${process.env.API_BASE_URL}customer`, {
-            data: {
-                email: "test@swiftparcel.com",
-                fullName: "test trial",
-                phoneNumber: "323232232",
-                password: "lucky689",
-            },
-        });
-
-        const loginResponse = await request.post(
-            `${process.env.API_BASE_URL}auth/login`,
-            {
-                data: {
-                    email: "test@swiftparcel.com",
-                    password: "lucky689",
-                },
-            },
+        const customerId = await returnCustomerId(
+            `pricing2${process.env.API_USERNAME}`,
+            request,
+            accessTokenOne,
         );
-        const loginResponseBody = await loginResponse.json();
-        const newCustomerToken = loginResponseBody.access_token;
-
         pickupRequestDetails.serviceType = 1;
         pickupRequestDetails.parcelWeight = 10;
 
         await request.post(
-            `${process.env.API_BASE_URL}customer/5/pickup-requests`,
+            `${process.env.API_BASE_URL}customer/${customerId}/pickup-requests`,
             {
                 headers: {
-                    Authorization: `Bearer ${newCustomerToken}`,
+                    Authorization: `Bearer ${accessTokenTwo}`,
                 },
                 data: pickupRequestDetails,
             },
         );
 
         const response = await request.post(
-            `${process.env.API_BASE_URL}pricing/quotes/6`,
+            `${process.env.API_BASE_URL}pricing/quotes/9`,
             {
                 headers: {
-                    Authorization: `Bearer ${newCustomerToken}`,
+                    Authorization: `Bearer ${accessTokenTwo}`,
                 },
             },
         );
@@ -283,12 +472,14 @@ test.describe("Pricing tests - positive", {}, () => {
 
 test.describe("Pricing tests - security", { tag: "@security" }, () => {
     test("Price calculation with missing authentication token", async ({
-        apiBaseUrl,
         request,
     }) => {
-        const response = await request.post(`${apiBaseUrl}pricing`, {
-            data: pickupRequestDetails,
-        });
+        const response = await request.post(
+            `${process.env.API_BASE_URL}pricing/quotes/4`,
+            {
+                data: pickupRequestDetails,
+            },
+        );
 
         let message: string;
         try {
@@ -300,19 +491,23 @@ test.describe("Pricing tests - security", { tag: "@security" }, () => {
         statusCodeCheck(response.status(), 401);
         expect
             .soft(message, "Error message for unauthorized user")
-            .toBe("Unauthorized user");
+            .toBe(
+                "Authentication failed: Full authentication is required to access this resource",
+            );
     });
 
     test("Price calculation with invalid authentication token", async ({
-        apiBaseUrl,
         request,
     }) => {
-        const response = await request.post(`${apiBaseUrl}pricing`, {
-            headers: {
-                Authorization: "invalid token",
+        const response = await request.post(
+            `${process.env.API_BASE_URL}pricing/quotes/4`,
+            {
+                headers: {
+                    Authorization: "invalid token",
+                },
+                data: pickupRequestDetails,
             },
-            data: pickupRequestDetails,
-        });
+        );
 
         let message: string;
         try {
@@ -324,6 +519,8 @@ test.describe("Pricing tests - security", { tag: "@security" }, () => {
         statusCodeCheck(response.status(), 401);
         expect
             .soft(message, "Error message for unauthorized user")
-            .toBe("Unauthorized user");
+            .toBe(
+                "Authentication failed: Full authentication is required to access this resource",
+            );
     });
 });
